efb5e303a275fbfb6669fbd9a84af6c140acdc7d
Jules Esnault <jules.esn@gmail.com>
2026-01-19 18:48:05 +0100

    PART[5-6]

--- main.cpp
+++ main.cpp
@@ -1,8 +1,15 @@
-#include "color.h"
-#include "ray.h"
-#include "vec3.h"
+#include "rtweekend.h"
 
-color ray_color(const ray& r){
+#include "hittable.h"
+#include "hittable_list.h"
+#include "sphere.h"
+
+color ray_color(const ray& r, const hittable& world) {
+    hit_record rec;
+    if (world.hit(r, interval(0, infinity), rec)) {
+        return 0.5 * (rec.normal + color(1,1,1));
+    }
+        
     vec3 unit_direction = unit_vector(r.direction());
     auto a = 0.5*(unit_direction.y() + 1.0);
     return (1.0-a)*color(1.0, 1.0, 1.0) + a*color(0.5, 0.7, 1.0);
@@ -15,6 +22,11 @@ int main(){
 
     int image_height = int( image_width / aspect_ratio);
     image_height = (image_height < 1) ? 1 : image_height;
+
+    hittable_list world;
+
+    world.add(make_shared<sphere>(point3(0,0,-1), 0.5));
+    world.add(make_shared<sphere>(point3(0,-100.5,-1), 100));
 
     auto focal_length = 1.0;
     auto viewport_height = 2.0;
@@ -40,7 +52,7 @@ int main(){
 
             ray r(camera_center, ray_direction);
 
-            color pixel_color = ray_color(r);
+            color pixel_color = ray_color(r, world);
             write_color(std::cout, pixel_color);
         }
     }
